d08907d6001bb9aba8de9004e13fe4af4b60499b
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-01-17 07:56:03 -0300

    Finalizando projeto

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,8 +1,11 @@
 import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente {
+  static numeroDeContas = 0;
   agencia;
   _cliente;
+  // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+  _saldo = 0;
 
   set cliente(novoValor) {
     if (novoValor instanceof Cliente) {
@@ -14,11 +17,14 @@ export class ContaCorrente {
     return this._cliente;
   }
 
-  // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
-  _saldo = 0;
-
   get saldo() {
     return this._saldo;
+  }
+
+  constructor(agencia, cliente) {
+    this.agencia = agencia;
+    this.cliente = cliente;
+    ContaCorrente.numeroDeContas += 1;
   }
 
   sacar(valor) {
